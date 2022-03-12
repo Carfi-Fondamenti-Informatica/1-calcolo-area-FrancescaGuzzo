cdcd2657336c22c83ba8181261ef41b976be0b6c
FrancescaGuzzo <100522082+FrancescaGuzzo@users.noreply.github.com>
2022-03-12 09:26:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
+float a, b, c;
+cout << "inserire tre valori reali\n";
 cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+float triangolo=(a*b)/2, quadrato=a*a, rettangolo=a*b, trapezio=((a+b)*c)/2;
+cout << "l'area del triangolo è " << triangolo << endl;
+cout << "l'area del quadrato è " << quadrato << endl;
+cout << "l'area del rettangolo è " << rettangolo << endl;
+cout << "l'area del trapezio è " << trapezio << endl;
 
-
+return 0;
 }
